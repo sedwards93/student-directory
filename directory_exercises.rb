f1ecf860e933de8c2124a6298f81957e61216f4d
Symion Edwards <symion.edwards@gmail.com>
2020-10-19 12:52:06 +0000

    Only prints students whose names are shorter than 12 characters

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -4,7 +4,7 @@ def print_header
 end
 def print(students)
     students.each_with_index do |student, index|
-        if student[:name][0] == "S"
+        if student[:name].length < 12
         puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
         end
     end
