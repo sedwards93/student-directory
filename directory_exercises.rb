caa5d800cd26a565cb480d09f94e941b350536c5
Symion Edwards <symion.edwards@gmail.com>
2020-10-22 10:41:46 +0000

    Added interactive menu to directory

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -70,7 +70,29 @@ def input_students
   students
 end
 
-students = input_students
-print_header
-prints(students)
-print_footer(students)
+def interactive_menu
+  students =[]
+  loop do
+    #print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # read the input and save it into a variable
+    selection = gets.chomp
+  # do what the user has asked
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    prints(students)
+    print_footer(students)
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant. Try again"
+  end
+  end
+end
+
+interactive_menu
