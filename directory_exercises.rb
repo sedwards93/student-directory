caa26bda32070f3101ad8e75e1ee326c34761d0b
Symion Edwards <symion.edwards@gmail.com>
2020-10-22 11:29:05 +0000

    Refactored interactive menu

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,98 +1,97 @@
-def print_header
-    linewidth = 60
-    puts (("The students of Villains Academy").center linewidth)
-    puts (("-------------").center linewidth)
-end
-
-def prints(students)
-  linewidth = 60
-  order = ["January", "February", "March", "April", "May", "June", "July",
-  "August", "September", "October", "November", "December"]
-  sorted = students.sort_by {|student| 
-  [order.index(student[:cohort])] 
-  }
-  if students.count != 0
-    sorted.each do |student|
-      puts ("#{student[:name]}:").ljust(linewidth/2) + ("(#{student[:cohort]} cohort)").rjust(linewidth/2)
-      puts ("Favourite hobby:").ljust(linewidth/2) + ("#{student[:hobbies]}").rjust(linewidth/2)
-      puts ("Country of birth: ").ljust(linewidth/2) + ("#{student[:country_of_birth]}").rjust(linewidth/2)
-      puts ("Student's Height: ").ljust(linewidth/2) + ("#{student[:height]}").rjust(linewidth/2)
-      puts "------------------------------------------------------------"
-    end
-  end
-end
-
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have 1 great student"
-  else
-    puts "Overall, we have #{names.count} great students"
-  end
-end
+@students = []
 
 def input_students
   puts "Please enter the name of the student, or hit return twice to finish"
-  students = []
-  name = gets
-  name = name.gsub(/\n/, "")
+  name = gets.chomp
   while !name.empty? do
   puts "What cohort is the student a member of?"
-  cohort = gets
-  cohort = cohort.gsub(/\n/, "")
+  cohort = gets.chomp
     while true
       months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", ""]
         if months.include?(cohort)
           break
         else
           puts "Please enter the cohort month again"
-          cohort = gets
-          cohort = cohort.gsub(/\n/, "")
+          cohort = gets.chomp
         end
     end
     if cohort == ""
     cohort = "October"
     end
     puts "Please enter the student's hobby"
-    hobby = gets
-    hobby = hobby.gsub(/\n/, "")
+    hobby = gets.chomp
     puts "Please enter the student's country of birth"
-    country = gets
-    country = country.gsub(/\n/, "")
+    country = gets.chomp
     puts "Please enter the student's height in cm"
-    height = gets
-    height = height.gsub(/\n/, "")
-      students << {name: name, cohort: cohort, hobbies: hobby, country_of_birth: country, height: height}
-      puts "Now we have #{students.count} students"
+    height = gets.chomp
+      @students << {name: name, cohort: cohort, hobbies: hobby, country_of_birth: country, height: height}
+      puts "Now we have #{@students.count} students"
       puts "Please enter the name of the next student, or hit return to finish"
-      name = gets
-      name = name.gsub(/\n/, "")
+      name = gets.chomp
   end
-  students
 end
 
-def interactive_menu
-  students =[]
-  loop do
-    #print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  # read the input and save it into a variable
-    selection = gets.chomp
-  # do what the user has asked
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
   case selection
   when "1"
-    students = input_students
+    input_students
   when "2"
-    print_header
-    prints(students)
-    print_footer(students)
+    show_students
   when "9"
     exit
   else
     puts "I don't know what you meant. Try again"
   end
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
+
+def print_header
+    linewidth = 60
+    puts (("The students of Villains Academy").center linewidth)
+    puts (("-------------").center linewidth)
+end
+
+def print_student_list
+  linewidth = 60
+    @students.each do |student|
+      puts ("#{student[:name]}:").ljust(linewidth/2) + ("(#{student[:cohort]} cohort)").rjust(linewidth/2)
+      puts ("Favourite hobby:").ljust(linewidth/2) + ("#{student[:hobbies]}").rjust(linewidth/2)
+      puts ("Country of birth: ").ljust(linewidth/2) + ("#{student[:country_of_birth]}").rjust(linewidth/2)
+      puts ("Student's Height: ").ljust(linewidth/2) + ("#{student[:height]}").rjust(linewidth/2)
+      puts "------------------------------------------------------------"
+    end
+end
+
+
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have 1 great student"
+  else
+    puts "Overall, we have #{@students.count} great students"
+  end
+end
+
+
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+
 
 interactive_menu
